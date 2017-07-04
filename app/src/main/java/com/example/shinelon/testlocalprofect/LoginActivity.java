a0d06f34ace1dae84ec4641824f52278613b2733
xiaoshuiz <shui_huachun@126.com>
2017-07-04 16:49:27 +0800

    httpUtil  finished

--- app/src/main/java/com/example/shinelon/testlocalprofect/LoginActivity.java
+++ app/src/main/java/com/example/shinelon/testlocalprofect/LoginActivity.java
@@ -68,9 +68,7 @@ public class LoginActivity extends AppCompatActivity {
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "18487210122:hello", "18754068484:world"
-    };
+
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
