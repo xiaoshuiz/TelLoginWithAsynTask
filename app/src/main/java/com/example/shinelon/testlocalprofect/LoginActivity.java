1def28f87dc186177c018899bc6c88358b6fd5dd
xiaoshuiz <shui_huachun@126.com>
2017-07-04 12:34:57 +0800

    httpUtil  finished

---
+++ app/src/main/java/com/example/shinelon/testlocalprofect/LoginActivity.java
@@ -0,0 +1,401 @@
+package com.example.shinelon.testlocalprofect;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import draw.CustomStatusView;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via tel/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "18487210122:hello", "18754068484:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+
+    // UI references.
+    private AutoCompleteTextView mTelView;
+    private EditText mPasswordView;
+    private View mLoginFormView;
+    private CustomStatusView mStatusView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mTelView = (AutoCompleteTextView) findViewById(R.id.tel);
+        populateAutoComplete();
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mTelSignInButton = (Button) findViewById(R.id.tel_sign_in_button);
+        mTelSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+
+        mStatusView = (CustomStatusView) findViewById(R.id.as_status);
+    }
+
+    private void populateAutoComplete() {
+        if (!mayRequestContacts()) {
+            return;
+        }
+
+
+    }
+
+    private boolean mayRequestContacts() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+            Snackbar.make(mTelView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        @TargetApi(Build.VERSION_CODES.M)
+                        public void onClick(View v) {
+                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+        return false;
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid tel, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mTelView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String tel = mTelView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid tel address.
+        if (TextUtils.isEmpty(tel)) {
+            mTelView.setError(getString(R.string.error_field_required));
+            focusView = mTelView;
+            cancel = true;
+        } else if (!isTelValid(tel)) {
+            mTelView.setError(getString(R.string.error_invalid_tel));
+            focusView = mTelView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            // showProgress(true);
+            showStatus(true);
+            mStatusView.loadLoading();
+
+            mAuthTask = new UserLoginTask(tel, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isTelValid(String tel) {
+        //TODO: Replace this with your own logic
+        return tel.length() == 11;
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+
+    private void showStatus(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mStatusView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+    public class HttpUtils {
+
+        OkHttpClient client = new OkHttpClient();
+        public final MediaType JSON
+                = MediaType.parse("application/json; charset=utf-8");
+
+        public String login(String url, String json) throws IOException {
+            //把请求的内容字符串转换为json
+            RequestBody body = RequestBody.create(JSON, json);
+            //RequestBody formBody = new FormEncodingBuilder()
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            String result = response.body().string();
+
+
+            return result;
+
+
+        }
+
+
+        public String bolwingJson(String username, String password) {
+            return "{'username':" + username + "," + "'password':" + password + "}";
+            //     "{'username':" + username + ","+"'password':"+password+"}";
+        }
+
+
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, String, Boolean> {
+
+        private final String mTel;
+        private final String mPassword;
+
+
+        UserLoginTask(String tel, String password) {
+            mTel = tel;
+            mPassword = password;
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            String m = "0";//to finish the threadfail
+
+            HttpUtils httpUtils = new HttpUtils();
+            //转换为JSON
+            String user = httpUtils.bolwingJson(mTel, mPassword);
+
+
+            //String user ="{'username':" + username + ","+"'password':"+password+"}";
+
+
+            try {
+                final String result = httpUtils.login(getString(R.string.loginurl), user);
+                if (result.equals("1")) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                } else {
+                    try {
+                        Thread.sleep(2000);
+                        // Simulate network access.
+                        publishProgress(m);
+                        Thread.sleep(2000);
+
+                    } catch (Exception e) {
+
+                    }
+                    return false;
+
+                }
+                //更新UI,在UI线程中-+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+                publishProgress(m);
+                Thread.sleep(2000);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+
+            // TODO: register the new account here.
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            if (values[0].equals("0")) {
+                mStatusView.initPath();
+                mStatusView.loadFailure();
+
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showStatus(true);
+
+            if (success) {
+                mStatusView.initPath();
+                mStatusView.loadSuccess();
+
+            } else {
+
+
+                showStatus(false);
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showStatus(false);
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/draw/StatusEnum.java
@@ -0,0 +1,11 @@
+package draw;
+
+/**
+ * Created by shuiz on 2017/7/3.
+ */
+
+public enum StatusEnum {
+    Loading,
+    LoadSuccess,
+    LoadFailure
+}
